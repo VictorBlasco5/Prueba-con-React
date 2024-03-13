258279955a84907432ecd09028febc61c3c32b46
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-03-13 19:05:26 +0100

    feat: project base drop-down menu

---
+++ src/common/CustomLink/CustomLink.jsx
@@ -0,0 +1,13 @@
+import './CustomLink.css'
+import { useNavigate } from 'react-router-dom'
+
+export const CustomLink = ({title, destination}) => {
+
+    const navigate = useNavigate()
+
+     return (
+        <div className='linkDesign' onClick={() => navigate(destination)}>
+            {title}
+        </div>
+     )
+}
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,7 @@
+import './Register.css'
+
+export const Register = () => {
+     return (
+        <div className='registerDesign'></div>
+     )
+}
